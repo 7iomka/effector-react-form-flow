bd07041f1c660f2c6b87024d71479ef494518c14
7iomka <7iomka@gmail.com>
2022-10-05 22:19:30 +0000

    Update 3 files

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,13 @@ const CustomWrapper = () => {
   return (
     <Text mb={15} className="text-center">
       <div
-        className="mx-auto px-4 md:px-8 max-w-screen-xl"
-        style={{ maxWidth: "400px" }}
+        style={{
+          maxWidth: "400px",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
       >
-        <div className="prose text-xl lg:text-2xl text-left">
+        {/* <div className="prose text-xl lg:text-2xl text-left">
           Floating UI is a low-level toolkit to create{" "}
           <Tooltip
             label="Floating element is one that floats on top of the UI without disrupting the flow of content, like this one!"
@@ -16,12 +19,34 @@ const CustomWrapper = () => {
             withArrow
             width={450}
             multiline
+            inline
           >
             <span tabIndex={0} className="relative underline">
               floating elements
             </span>
           </Tooltip>
           . Tooltips, popovers, dropdowns, menus, and more.
+        </div> */}
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div> Floating UI is a low-level toolkit to dblabllalvav </div>
+          <Tooltip
+            label="Promo text will be here"
+            color="blue"
+            position="bottom"
+            multiline
+            withArrow
+            transition="fade"
+            inline
+          >
+            <span style={{ marginLeft: "25px" }} tabIndex={0}>
+              (i)
+            </span>
+          </Tooltip>
         </div>
       </div>
     </Text>
